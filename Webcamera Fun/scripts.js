0529fd13600ec0c39ab51a79be4a312cb8d50726
snork-maiden <pollykoldysheva@gmail.com>
2022-08-15 17:03:27 +0300

    Add filters, design fixes, add mobile version

--- Webcamera Fun/scripts.js
+++ Webcamera Fun/scripts.js
@@ -8,7 +8,6 @@ function getVideo() {
   navigator.mediaDevices
     .getUserMedia({ video: true, audio: false })
     .then((localMediaStream) => {
-      console.log(localMediaStream);
       video.srcObject = localMediaStream;
       video.play();
     });
@@ -22,9 +21,16 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+
+    let levels = {};
+
+    document.querySelectorAll(".range").forEach((input) => {
+      levels[input.name] = +input.value;
+    });
+
+    ctx.globalAlpha = Math.abs(levels.globalAlpha) / 10;
     let pixels = ctx.getImageData(0, 0, width, height);
-    pixels = greenScreen(pixels);
-    // ctx.globalAlpha = 0.2;
+    pixels = rgbSplit(pixels, levels);
     ctx.putImageData(pixels, 0, 0);
   }, 16);
 }
@@ -37,56 +43,19 @@ function takePhoto() {
   const link = document.createElement("a");
   link.href = data;
   link.setAttribute("download", "handsome");
-  link.innerHTML = `<img src="${data}" alt="Beauty">`;
+  link.innerHTML = `<img class="strip__image" src="${data}" alt="Beauty">`;
   strip.insertBefore(link, strip.firstChild);
 }
 
-function redEffect(pixels) {
+function rgbSplit(pixels, levels) {
   for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i] += 100; //RED
-    pixels.data[i + 1] -= 50; //GREEN
-    pixels.data[i + 2] *= 0.5; //BLUE
+    pixels.data[i + levels.red] = pixels.data[i + 0]; // RED
+    pixels.data[i + levels.green + 1] = pixels.data[i + 1]; // GREEN
+    pixels.data[i + levels.blue + 2] = pixels.data[i + 2]; // Blue
   }
   return pixels;
 }
 
-function rgbSplit(pixels) {
-  for (let i = 0; i < pixels.data.length; i += 4) {
-    pixels.data[i - 300] = pixels.data[i + 0]; // RED
-    pixels.data[i + 200] = pixels.data[i + 1]; // GREEN
-    pixels.data[i - 450] = pixels.data[i + 2]; // Blue
-  }
-  return pixels;
-}
-
-function greenScreen(pixels) {
-  const levels = {};
-
-  document.querySelectorAll(".rgb input").forEach((input) => {
-    levels[input.name] = input.value;
-  });
-
-  for (i = 0; i < pixels.data.length; i = i + 4) {
-    red = pixels.data[i + 0];
-    green = pixels.data[i + 1];
-    blue = pixels.data[i + 2];
-    alpha = pixels.data[i + 3];
-
-    if (
-      red >= levels.rmin &&
-      green >= levels.gmin &&
-      blue >= levels.bmin &&
-      red <= levels.rmax &&
-      green <= levels.gmax &&
-      blue <= levels.bmax
-    ) {
-      // take it out!
-      pixels.data[i + 3] = 0;
-    }
-  }
-
-  return pixels;
-}
 
 getVideo();
 video.addEventListener("canplay", paintToCanvas);
